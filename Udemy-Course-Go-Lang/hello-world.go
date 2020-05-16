bc8d89a53b5ab4adb3ec4197b9cc4b955e807440
Chaitanya D <csdeshpande19@gmail.com>
2020-05-17 00:19:45 +0530

    Updated hello-word.go

--- Udemy-Course-Go-Lang/hello-world.go
+++ Udemy-Course-Go-Lang/hello-world.go
@@ -4,5 +4,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hi, I'm new to Go.")
+	foo()
+	bar()
+	fmt.Println("Bye.")
+}
+
+func foo() {
+	fmt.Println("In Foo")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+func bar() {
+	fmt.Println("In Bar")
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 }

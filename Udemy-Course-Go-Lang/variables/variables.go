da46928f56eb7d73fe8d54233997fef77fa6b572
Chaitanya D <csdeshpande19@gmail.com>
2020-05-17 19:47:19 +0530

    Added varibles.go

---
+++ Udemy-Course-Go-Lang/variables/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x int
+
+func main() {
+	// x := 10
+	x = 34
+	var y float32 = 20.23
+	var b int = 4
+	var e float32 = y
+	var f string = "hello"
+	fmt.Println(x, y, b, e, f)
+}
